1a92dfee7b8e4ec815810cdfa11f7f7e2befde42
Tracy Isman <tracyisman@gmail.com>
2024-03-12 12:19:18 -0400

    modularize server4 quiz done

--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -2,3 +2,17 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path');
 const router = express.Router()
+
+router.use(express.json());
+
+router.post('/write/adduser', (req, res) => {
+    let newuser = req.body;
+    req.users.push(newuser);
+    fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users), (err) => {
+        if (err) console.log('Failed to write');
+        else console.log('User Saved');
+    });
+    res.send('done');
+})
+
+module.exports = router
